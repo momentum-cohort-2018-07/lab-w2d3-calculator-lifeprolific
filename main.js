d7172178be3b3ec620d51471fc4d20a32c5944dc
lifeprolific <thegregoman@gmail.com>
2018-08-08 16:33:21 -0400

    reset equals after explosion

--- main.js
+++ main.js
@@ -4,37 +4,51 @@ var execute = document.getElementById("execute")
 var display = document.getElementById("display")
 
 var buttonDictionary = {
-    b1:"1",
-    b2:"2",
-    b3:"3",
-    b4:"4",
-    b5:"5",
-    b6:"6",
-    b7:"7",
-    b8:"8",
-    b9:"9",
-    b0:"0",
-    decimal:".",
-    divide:"/",
-    multiply:"*",
-    subtract:"-",
-    add:"+",
+    b1: "1",
+    b2: "2",
+    b3: "3",
+    b4: "4",
+    b5: "5",
+    b6: "6",
+    b7: "7",
+    b8: "8",
+    b9: "9",
+    b0: "0",
+    decimal: ".",
+    divide: "/",
+    multiply: "*",
+    subtract: "-",
+    add: "+",
 }
 
-function clickText(event) {
-    display.innerHTML = display.innerHTML + buttonDictionary[this.id]
+var validKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "/", "*", "-", "+"]
+
+function checkValid () {
     try {
         eval(display.innerHTML)
         execute.style.backgroundColor = "lightseagreen"
     }
     catch (error) {
-        console.log("error detected")
         execute.style.backgroundColor = "red"
+    }
+}
+
+function clickText(event) {
+    display.innerHTML = display.innerHTML + buttonDictionary[this.id]
+    checkValid()
+}
+
+function typeText(event) {
+    var keyPressed = event.key
+    if (validKeys.includes(keyPressed)) {
+        display.innerHTML = display.innerHTML + keyPressed
+        checkValid()
     }
 }
 
 function clearDisplay(event) {
     display.innerHTML = ""
+    execute.style.backgroundColor = "lightseagreen"
 }
 
 function evaluate(event) {
@@ -43,26 +57,22 @@ function evaluate(event) {
             display.innerHTML = eval(display.innerHTML)
         }
     }
-    catch(error) {
+    catch (error) {
         display.innerHTML = "<img src='./explosion.gif' id='explosionImg'>"
-        explosionTimeout = window.setTimeout(removeExplosion,1000)
+        explosionTimeout = window.setTimeout(removeExplosion, 1000)
     }
 }
 
 function removeExplosion() {
     display.innerHTML = ""
     window.clearTimeout(explosionTimeout)
+    execute.style.backgroundColor = "lightseagreen"
 }
 
+//add event listeners
 for (inputButton of inputButtons) {
     inputButton.addEventListener("click", clickText)
 }
-
 clear.addEventListener("click", clearDisplay)
-
 execute.addEventListener("click", evaluate)
-
-document.addEventListener('keypress', (event) => {
-    const keyName = event.key
-    display.innerHTML = display.innerHTML + keyName
-  })
+document.addEventListener('keypress', typeText)
